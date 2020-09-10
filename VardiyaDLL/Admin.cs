1674b79e6e473af74b0930ac7a27291d4781f502
canka <cankaplanxo@gmail.com>
2020-09-10 20:26:18 +0300

    first commit

---
+++ VardiyaDLL/Admin.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VardiyaDLL
+{
+
+    public class Admin
+    {
+        string kullaniciadi, sifre, adisoyadi;
+        DateTime sistemegiris, kayittarihi;
+        int kullaniciid;
+        byte durumu;
+        public string KullaniciAdi
+        {
+            get { return kullaniciadi; }
+            set { kullaniciadi = value; }
+        }
+        public string Sifre
+        {
+            get { return sifre; }
+            set { sifre = value; }
+        }
+
+        public int KullaniciID
+        {
+            get { return kullaniciid; }
+            set { kullaniciid = value; }
+        }
+
+        public string AdiSoyadi
+        {
+            get { return adisoyadi; }
+            set { adisoyadi = value; }
+        }
+
+        public byte Durumu
+        {
+            get { return durumu; }
+            set { durumu = value; }
+        }
+
+        public DateTime KayitTarihi
+        {
+            get { return kayittarihi; }
+            set { kayittarihi = value; }
+        }
+        public DateTime SistemGiris
+        {
+            get { return sistemegiris; }
+            set { sistemegiris = value; }
+        }
+    }
+
+}
---
+++ VardiyaDLL/Personel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VardiyaDLL
+{
+    public class Personel
+    {
+        string kullaniciadi, sifre, adisoyadi,sicilno,vardiyasaat,durak;
+        DateTime vardiyatarih;
+        int kullaniciid;
+        byte durumu;
+        public string SicilNo
+        {
+            get { return sicilno; }
+            set { sicilno = value; }
+        }
+        public string VardiyaSaati
+        {
+            get { return vardiyasaat; }
+            set { vardiyasaat = value; }
+        }
+
+        public DateTime VardiyaTarihi
+        {
+            get { return vardiyatarih; }
+            set { vardiyatarih = value; }
+        }
+
+        public string Durak
+        {
+            get { return durak; }
+            set { durak = value; }
+        }
+
+        public byte Durumu
+        {
+            get { return durumu; }
+            set { durumu = value; }
+        }
+
+    }
+}
